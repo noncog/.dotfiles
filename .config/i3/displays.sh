279cb7622f485d756036ee9f939c54761bab9338
noncog <108415765+noncog@users.noreply.github.com>
2022-07-05 11:16:23 -0400

    Updated display names

--- .config/i3/displays.sh
+++ .config/i3/displays.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# make display available
+xrandr --auto
 # rotate laptop display
-xrandr --output eDP-1 --rotate right --auto
+xrandr --output eDP-1-1 --rotate right --auto
 # set monitor positions
-xrandr --output eDP-1 --left-of HDMI-1-0
+xrandr --output eDP-1-1 --left-of HDMI-0
 # set primary display
-xrandr --output HDMI-1-0 --primary
+xrandr --output HDMI-0 --primary
 # set refresh rate of primary
-xrandr --output HDMI-1-0 --mode 2560x1440 --rate 143.91
+xrandr --output HDMI-0 --mode 2560x1440 --rate 143.91
 # set backgrounds
 nitrogen --restore
