a4bbd6a4957305566ba9cd8e5aa0171053177d86
noncog <108415765+noncog@users.noreply.github.com>
2022-07-12 02:46:20 -0400

    Updated org-bullets to custom list

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -87,7 +87,10 @@
 
 (use-package org-bullets
   :after org                                          ; ensures org-bullets package is loaded after org package
-  :hook (org-mode . org-bullets-mode))                ; start bullets-mode after org-mode
+  :hook (org-mode . org-bullets-mode)                 ; start bullets-mode after org-mode
+  :config
+  (setq org-bullets-bullet-list '("Ͻ" "✖" "➖" "❯" "○" "➡" "➕" "⬤"))
+  )
 
 (use-package org-noter
   :config
@@ -173,16 +176,3 @@
   :config
   (load-theme 'dracula t)
   )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(dracula-theme which-key dashboard projectile pdf-tools ivy-rich counsel org-noter org-bullets org delight use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
