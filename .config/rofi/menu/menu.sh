d5d75b934ae0a42becf352eb99c78a4ec13b098e
noncog <108415765+noncog@users.noreply.github.com>
2022-07-25 20:59:42 -0400

    Updated menu format and added rofi-borg

--- .config/rofi/menu/menu.sh
+++ .config/rofi/menu/menu.sh
@@ -22,45 +22,45 @@ options="$screenshot\n$brightness\n$configuration\n$backups\n$settings\n$power"
 chosen="$(echo -e "$options" | $rofi_command -p 'menu' -dmenu -selected-row 1)"
 case $chosen in
     $screenshot)
-		if [[ -f "$dir/screenshot.sh" ]]; then
-		    bash "$dir/screenshot.sh"
-		else
-			msg "$screenshot file not found"
-		fi
-        ;;
+	if [[ -f "$dir/screenshot.sh" ]]; then
+	    bash "$dir/screenshot.sh"
+	else
+	    msg "$screenshot file not found"
+	fi
+	;;
     $brightness)
-		if [[ -f "$dir/brightness.sh" ]]; then
-		    bash "$dir/brightness.sh"
-		else
-			msg "@brightness file not found"
-		fi
+	if [[ -f "$dir/brightness.sh" ]]; then
+	    bash "$dir/brightness.sh"
+	else
+	    msg "@brightness file not found"
+	fi
         ;;
     $configuration)
-		if [[ -f "$dir/configuration.sh" ]]; then
-			bash "$dir/configuration.sh"
-		else
-			msg "@configuration file not found"
-		fi
+	if [[ -f "$dir/configuration.sh" ]]; then
+	    bash "$dir/configuration.sh"
+	else
+	    msg "@configuration file not found"
+	fi
         ;;
-	$backups)
-		if [[ -f "$dir/backups.sh" ]]; then
-			bash "$dir/backups.sh"
-		else
-			msg "@backups file not found"
-		fi
+    $backups)
+	if [[ -f "$dir/rofi-borg/rofi-borg.sh" ]]; then
+	    bash "$dir/rofi-borg/rofi-borg.sh"
+	else
+	    msg "@backups file not found"
+	fi
         ;;
-	$settings)
-		if [[ -f "$dir/settings.sh" ]]; then
-			bash "$dir/settings.sh"
-		else
-			msg "@settings file not found"
-		fi
+    $settings)
+	if [[ -f "$dir/settings.sh" ]]; then
+	    bash "$dir/settings.sh"
+	else
+	    msg "@settings file not found"
+	fi
         ;;
-	$power)
-		if [[ -f "$dir/power.sh" ]]; then
-			bash "$dir/power.sh"
-		else
-			msg "@power file not found"
-		fi
+    $power)
+	if [[ -f "$dir/power.sh" ]]; then
+	    bash "$dir/power.sh"
+	else
+	    msg "@power file not found"
+	fi
         ;;
 esac
