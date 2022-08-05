3707d99fbbfdfd17435ee89a273f1c87249edefa
noncog <108415765+noncog@users.noreply.github.com>
2022-08-05 03:22:51 -0400

    Added settings for transparent bars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,10 @@
 killall -q polybar
 
 # Launch default bar and/or others
-echo "---" | tee -a /tmp/polybar_default_bar.log
+echo "---" | tee -a /tmp/polybar_default_bar.log /tmp/polybar_clear.log /tmp/polybar_left.log /tmp/polybar_center.log /tmp/polybar_right.log
 polybar default_bar 2>&1 | tee -a /tmp/polybar_default_bar.log & disown
-
+#polybar clear 2>&1 | tee -a /tmp/polybar_transparent.log & disown
+#polybar left 2>&1 | tee -a /tmp/polybar_left.log & disown
+#polybar center 2>&1 | tee -a /tmp/polybar_center.log & disown
+#polybar right 2>&1 | tee -a /tmp/polybar_right.log & disown
 echo "Bars launched..."
