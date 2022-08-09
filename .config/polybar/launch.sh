c1fe17f85d6ece16d7e2a88637ea218447f47f5e
noncog <108415765+noncog@users.noreply.github.com>
2022-08-09 18:03:00 -0400

    Removed transparent bar related stuff

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,10 +7,6 @@
 killall -q polybar
 
 # Launch default bar and/or others
-echo "---" | tee -a /tmp/polybar_default_bar.log #/tmp/polybar_clear.log /tmp/polybar_left.log /tmp/polybar_center.log /tmp/polybar_right.log
+echo "---" | tee -a /tmp/polybar_default_bar.log 
 polybar default_bar 2>&1 | tee -a /tmp/polybar_default_bar.log & disown
-#polybar clear 2>&1 | tee -a /tmp/polybar_transparent.log & disown
-#polybar left 2>&1 | tee -a /tmp/polybar_left.log & disown
-#polybar center 2>&1 | tee -a /tmp/polybar_center.log & disown
-#polybar right 2>&1 | tee -a /tmp/polybar_right.log & disown
 echo "Bars launched..."
