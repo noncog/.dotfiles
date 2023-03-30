5fd60c3d2cd44525808cf3b459aa300ad4175689
noncog <108415765+noncog@users.noreply.github.com>
2023-03-30 00:45:54 -0400

    feat: add auto resizing to sketchybar
    Added a helper function to sketchybar which finds the current menu bar
    height. This is run on launch. Added a yabai signal to detect when the
    resolution is changed and tell sketchybar to restart when this happens.
    
    This updates the bar height to be the height of the notch regardless of
    the resolution.

---
+++ .config/sketchybar/helpers/get_menu_bar_height.m
@@ -0,0 +1,19 @@
+#import <Cocoa/Cocoa.h>
+
+int main() {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    NSRect screenRect = [[NSScreen mainScreen] frame];
+    NSRect visibleRect = [[NSScreen mainScreen] visibleFrame];
+
+    // Show the menu bar temporarily to get its height
+    [NSMenu setMenuBarVisible:YES];
+
+    CGFloat menuBarHeight = screenRect.size.height - visibleRect.size.height;
+    printf("%d\n", (int) menuBarHeight);
+
+    // Hide the menu bar again
+    [NSMenu setMenuBarVisible:NO];
+
+    [pool drain];
+    return 0;
+}
