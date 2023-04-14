779a11f2dd01b91acd7603e9088156e5aaa009f3
noncog <108415765+noncog@users.noreply.github.com>
2023-04-14 02:38:28 -0400

    feat: make font settings be system dependent
    Could eventually do what Tecosaur did and automatically ensure that the
    fonts are present. For now, this will be enough.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -26,11 +26,21 @@
 
 (setq doom-dracula-colorful-headers t)
 
-(setq doom-font (font-spec :family "Jetbrains Mono" :size 12)
-     doom-big-font (font-spec :family "Jetbrains Mono" :size 16)
-     doom-variable-pitch-font (font-spec :family "Overpass" :size 14)
-     doom-unicode-font (font-spec :family "JuliaMono")
-     doom-serif-font (font-spec :family "IBM Plex Mono" :size 10 :weight 'light))
+(when IS-MAC
+  (setq doom-font (font-spec :family "Jetbrains Mono" :size 12)
+        doom-big-font (font-spec :family "Jetbrains Mono" :size 16)
+        doom-variable-pitch-font (font-spec :family "Overpass" :size 14)
+        doom-unicode-font (font-spec :family "JuliaMono")
+        doom-serif-font (font-spec :family "IBM Plex Mono" :size 10 :weight 'light)))
+
+(when (eq system-type "gnu/linux")
+  (setq doom-font (font-spec :family "Jetbrains Mono" :size 12)
+        doom-big-font (font-spec :family "Jetbrains Mono" :size 16)
+        ;;doom-variable-pitch-font (font-spec :family "Overpass" :size 14)
+        ;;doom-unicode-font (font-spec :family "JuliaMono")
+        ;;doom-serif-font (font-spec :family "IBM Plex Mono" :size 10 :weight 'light)
+        )
+)
 
 ;(add-to-list 'default-frame-alist '(alpha . 93)) ; [0-100]
 
