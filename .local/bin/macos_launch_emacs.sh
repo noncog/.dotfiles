5019960cd45346e10860ea536f7b552da1164490
noncog <108415765+noncog@users.noreply.github.com>
2023-10-21 20:00:21 -0400

    feat: update macos emacs launcher

--- .local/bin/macos_launch_emacs.sh
+++ .local/bin/macos_launch_emacs.sh
@@ -3,11 +3,8 @@
 # This script enables one keybind to launch the daemon/client for Emacs
 # if it doesn't exist or to focus an existing frame if it does exist.
 
-# Get the default Chemacs2 profile
-read -r default_profile <"$HOME/.config/chemacs/profile"
-
 # Check if the server for that profile is running and if it is check if it has a frame.
-frame_check=$(emacsclient -s $default_profile -e "(> (length (frame-list)) 1)")
+frame_check=$(emacsclient -e "(> (length (frame-list)) 1)")
 
 # if it is running and we have frame, then focus it.
 if [ $? ] && [ "$frame_check" = "t" ]; then
@@ -15,5 +12,6 @@ if [ $? ] && [ "$frame_check" = "t" ]; then
     exit 0
 else
     # if not running or have no frame, then create a frame/daemon.
-    emacsclient -n -c -s $default_profile -a=''
+    emacsclient -c -n -a='' && \
+    osascript -e "tell application \"Emacs\" to activate"
 fi
