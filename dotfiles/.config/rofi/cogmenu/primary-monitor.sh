609a9503aba96b5edb1fc6592a77a317a3267ec3
noncog <108415765+noncog@users.noreply.github.com>
2024-10-01 07:49:05 -0400

    feat: update monitor dpi for main display

--- dotfiles/.config/rofi/cogmenu/primary-monitor.sh
+++ dotfiles/.config/rofi/cogmenu/primary-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # make display available
 xrandr --auto
-xrandr --dpi 96
+xrandr --dpi 108
 xrandr --output DP-0 --primary --mode 2560x1440 --rate 165.08
 xrandr --output HDMI-0 --off
 # set backgrounds
--- dotfiles/.config/rofi/cogmenu/two-monitor.sh
+++ dotfiles/.config/rofi/cogmenu/two-monitor.sh
@@ -3,8 +3,7 @@
 #!/bin/bash
 # make display available
 xrandr --auto
-
-xrandr --auto
+xrandr --dpi 108
 xrandr --output DP-0 --primary --mode 2560x1440 --rate 165.08
 xrandr --output HDMI-0 --right-of DP-0 --mode 1920x1080 --rate 60.00
 # set backgrounds
