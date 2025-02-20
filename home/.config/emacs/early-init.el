6c126c656b41e4dab4168ffbf55f9cf32f1977a6
noncog <108415765+noncog@users.noreply.github.com>
2025-02-20 06:05:44 -0500

    feat(emacs): initialize vanilla configuration
    I'm switching from Doom and building my own configuration. This includes
    controlling the version of Emacs on the system. This commit includes
    compilation instructions to avoid Debian's dated version.

---
+++ home/.config/emacs/early-init.el
@@ -0,0 +1,20 @@
+;;; early-init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Created: February 20, 2025
+;; Modified: February 20, 2025
+;; Version: 0.0.1
+;; Homepage: https://github.com/noncog/early-init
+;; Package-Requires: ((emacs "29.4"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'early-init)
+;;; early-init.el ends here
