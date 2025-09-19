75f1091814f962e0c74461d16ad49b47748f8ed2
noncog <108415765+noncog@users.noreply.github.com>
2025-09-19 01:46:27 -0400

    fix(org-file): ensure functions in narrowed buffer
    The previous implementation only worked in the active buffer region even
    if narrowed. It also had logic issues in some scenarios. This
    implementation is split into two functions and appears to more reliably
    complete all checks, even in narrowed regions.

--- home/.config/doom/lisp/lib/org-file.el
+++ home/.config/doom/lisp/lib/org-file.el
@@ -48,48 +48,54 @@ explicitly set to nil, use string \"nil\" for VALUE."
       (if (and has-prop (progn (goto-char has-prop) (org-before-first-heading-p)))
           (if value t (org-entry-get has-prop property nil t)) nil))))
 
-;; TODO: Ensure works in narrowed buffers.
-;; TODO: Replace org-file-property-p argument for value a boolean and not string.
-;; TODO: Consider support for passing in filename.
-;; NOTE: Could replace with org-ql, ripgrep, or custom parser for performance.
+(defun org-file-agenda-elements-p ()
+  "Return non-nil if current file buffer has elements considered for the agenda.
+
+Searches for entries in the active buffer containing:
+- Active timestamps under headings.
+- TODO keywords found in `org-file-agenda-keywords'.
+- Tags found in `org-file-agenda-tags'."
+  (when (org-element-map (org-element-parse-buffer 'headline) 'headline
+          (lambda (h)
+            (let ((todo (org-element-property :todo-keyword h))
+                  (todo-type (org-element-property :todo-type h)))
+              (or (when org-file-agenda-keywords
+                    ;; Any headline with selected todo keywords.
+                    ;; Could add fallback to keywords of type todo.
+                    (seq-contains-p org-file-agenda-keywords todo))
+                  (when org-file-agenda-tags
+                    ;; Any headline with selected tags.
+                    (seq-intersection (org-element-property :tags h) org-file-agenda-tags))
+                  (and
+                   ;; Any non-todo headline with an active timestamp.
+                   (not (eq 'done todo-type))
+                   (org-element-property :contents-begin h)
+                   (save-excursion
+                     (goto-char (org-element-property :contents-begin h))
+                     ;; Must look for active timestamps only before next
+                     ;; heading, even if it's a child, but org-element-property
+                     ;; :contents-end includes all children.
+                     (let ((end (save-excursion
+                                  (or (re-search-forward
+                                       org-element-headline-re
+                                       (org-element-property :contents-end h) ':noerror)
+                                      (org-element-property :contents-end h)))))
+                       (re-search-forward org-ts-regexp end 'noerror))))))) nil 'first-match) t))
+
 (defun org-file-agenda-p ()
   "Return non-nil if current file buffer should be considered for the agenda.
 
-Searches for entries containing:
-- Active timestamps.
-- TODO keywords found in `org-file-agenda-keywords'.
-- Tags found in `org-file-agenda-tags'."
+Searches for elements considered by `org-file-agenda-elements-p'.
+
+Force inclusion or exclusion with file property `AGENDA' set to t or nil."
   (interactive)
-  ;; Return immediately if 'agenda' file property is nil, meaning don't include.
-  (if (org-file-property-p "agenda" "nil") nil
-    (when (org-element-map
-              (org-element-parse-buffer 'headline) 'headline
-            (lambda (h)
-              (let ((todo (org-element-property :todo-keyword h))
-                    (todo-type (org-element-property :todo-type h)))
-                (or (when org-file-agenda-keywords
-                      ;; Any headline with selected todo keywords.
-                      ;; Could add fallback to keywords of type todo.
-                      (seq-contains-p org-file-agenda-keywords todo))
-                    (when org-file-agenda-tags
-                      ;; Any headline with selected tags.
-                      (seq-intersection (org-element-property :tags h) org-file-agenda-tags))
-                    ;; Any non-todo headline with an active timestamp.
-                    (and
-                     (not (eq 'done todo-type))
-                     (org-element-property :contents-begin h)
-                     (save-excursion
-                       (goto-char (org-element-property :contents-begin h))
-                       ;; Must look for active timestamps only
-                       ;; before next heading, even if it's a
-                       ;; child, but org-element-property
-                       ;; :contents-end includes all children.
-                       (let ((end (save-excursion
-                                    (or (re-search-forward
-                                         org-element-headline-re
-                                         (org-element-property :contents-end h) ':noerror)
-                                        (org-element-property :contents-end h)))))
-                         (re-search-forward org-ts-regexp end 'noerror))))))) nil 'first-match) t)))
+  (let ((agenda-property (org-file-property-p "agenda")))
+    (cond ((and agenda-property (string= "t" agenda-property)) t)
+          ((and agenda-property (string= "nil" agenda-property)) nil)
+          (t (if (buffer-narrowed-p)
+                 (org-with-wide-buffer
+                  (org-file-agenda-elements-p))
+               (org-file-agenda-elements-p))))))
 
 (defun org-file-end-of-meta-data ()
   "Skip to end of properties drawer and keywords in file.
