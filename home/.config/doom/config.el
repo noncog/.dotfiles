3409830c088b46d38481107e269329a4dfbf8c4f
noncog <108415765+noncog@users.noreply.github.com>
2026-06-02 02:18:23 -0400

    fix: use use-package over doom macro

--- home/.config/doom/config.el
+++ home/.config/doom/config.el
@@ -91,7 +91,7 @@
   (setq doom-font (font-spec :family "JetBrains Mono" :size 11.0)
         doom-big-font (font-spec :family "JetBrains Mono" :size 15.0)))
 
-(use-package! display-line-numbers
+(use-package display-line-numbers
   :defer t
   :config
   (setq display-line-numbers-type 'visual
