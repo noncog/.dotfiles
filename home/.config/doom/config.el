4f166589e3dc8c6fb882bcc1b78304fa0134cb15
noncog <108415765+noncog@users.noreply.github.com>
2026-06-02 02:13:12 -0400

    feat: configure line numbers

--- home/.config/doom/config.el
+++ home/.config/doom/config.el
@@ -83,6 +83,14 @@
             "w K" #'windman-move-win-up
             "w L" #'windman-move-win-right)))
 
+;;; Interface
+
+(use-package! display-line-numbers
+  :defer t
+  :config
+  (setq display-line-numbers-type 'visual
+        display-line-numbers-grow-only t)
+  (add-hook 'org-mode-hook #'doom-disable-line-numbers-h))
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -108,10 +116,6 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
