c0e5f7cf6cd31dbf3298a1da2c7f1cbb795d07bb
noncog <108415765+noncog@users.noreply.github.com>
2026-06-23 14:21:01 -0500

    fix: add missing agent-shell packages
    Pushed the configuration without the package loading...

--- home/.config/doom/packages.el
+++ home/.config/doom/packages.el
@@ -3,3 +3,12 @@
 
 (package! vulpea
   :recipe (:host github :repo "d12frosted/vulpea"))
+
+(package! acp
+  :recipe (:host github :repo "xenodium/acp.el"))
+
+(package! shell-maker
+  :recipe (:host github :repo "xenodium/shell-maker"))
+
+(package! agent-shell
+  :recipe (:host github :repo "xenodium/agent-shell"))
