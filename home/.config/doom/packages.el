53b1c3884e6a1e7f10449f1ebd98be6a6cb01c9a
noncog <108415765+noncog@users.noreply.github.com>
2026-07-02 09:03:23 -0500

    fix: add missing 'note' packages

--- home/.config/doom/packages.el
+++ home/.config/doom/packages.el
@@ -12,3 +12,19 @@
 
 (package! agent-shell
   :recipe (:host github :repo "xenodium/agent-shell"))
+
+;; Experimental Includes:
+(package! vui.el
+  :recipe (:host github :repo "d12frosted/vui.el"))
+
+(package! vulpea-ui
+  :recipe (:host github :repo "d12frosted/vulpea-ui"))
+
+(package! vulpea-para
+  :recipe (:host github :repo "d12frosted/vulpea-para"))
+
+(package! vulpea-journal
+  :recipe (:host github :repo "d12frosted/vulpea-journal"))
+
+(package! denote
+  :recipe (:host github :repo "protesilaos/denote"))
